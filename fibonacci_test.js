239bc5bbebf83f180f29a1e386408078364c8dab
Artur Ferreira <artur.fer.cruz@gmail.com>
2025-11-14 10:43:36 +0100

    Update code coverage reports and enhance Fibonacci tests

--- fibonacci_test.js
+++ fibonacci_test.js
@@ -8,3 +8,25 @@ Deno.test("test first Fibonacci number", () => {
 Deno.test("test second Fibonacci number", () => {
   expect(fibonacci(1)).toBe(1);
 });
+
+Deno.test("test third Fibonacci number", () => {
+  expect(fibonacci(2)).toBe(2);
+});
+
+Deno.test("test fifth Fibonacci number", () => {
+  expect(fibonacci(5)).toBe(8);
+});
+Deno.test("test tenth Fibonacci number", () => {
+  expect(fibonacci(10)).toBe(89);
+});
+
+Deno.test("test negative input", () => {
+  expect(fibonacci(-1)).toBeUndefined();
+});
+
+Deno.test("test non-number input", () => {
+  expect(fibonacci("string")).toBeUndefined();
+});
+Deno.test("test null input returns undefined", () => {
+  expect(fibonacci(null)).toBeUndefined();
+});
